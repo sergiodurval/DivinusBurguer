54f01dd7f1c5ad3d0f43dd77e64b83e72b315cd8
sergio <sergiodurval2@gmail.com>
2019-03-27 23:52:35 -0300

    Iniciando o projeto

---
+++ backend/DivinusBurguer/Divinus.Domain/Entities/Food.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Divinus.Domain.Entities
+{
+    public class Food
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ImageName { get; set; }
+    }
+}
