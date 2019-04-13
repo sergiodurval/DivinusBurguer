507615c78f88e3b6832f97ce555a06b624139e3c
SergioDurval <sergiodurval2@gmail.com>
2019-04-13 13:58:35 -0300

    Criando a camada de infraestrutura

---
+++ backend/DivinusBurguer/Divinus.Infra/Persistence/DivinusContext.cs
@@ -0,0 +1,56 @@
+﻿using Divinus.Domain.Entities;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Divinus.Infra.Persistence
+{
+    public class DivinusContext : DbContext
+    {
+        public DivinusContext() : base("Divinus")
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public IDbSet<Food> Foods { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Seta o Schema default
+            //modelBuilder.HasDefaultSchema("Apoio");
+
+            //Remove a pluralização dos nomes das tabelas
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            //Remove exclusão em cascata
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            //Setar para usar varchar ou invés de nvarchar
+            modelBuilder.Properties<string>().Configure(p => p.HasColumnType("varchar"));
+
+            //Caso eu esqueça de informar o tamanho do campo ele irá colocar varchar de 100
+            modelBuilder.Properties<string>().Configure(p => p.HasMaxLength(100));
+
+            //Mapeia as entidades
+            //modelBuilder.Configurations.Add(new JogadorMap());
+            //modelBuilder.Configurations.Add(new PlataformaMap());
+
+            #region Adiciona entidades mapeadas - Automaticamente via Assembly
+            modelBuilder.Configurations.AddFromAssembly(typeof(DivinusContext).Assembly);
+            #endregion
+
+            #region Adiciona entidades mapeadas - Automaticamente via NameSpace
+            //Assembly.GetExecutingAssembly().GetTypes()
+            //    .Where(type => type.Namespace == "AlertaBrasil.Domain.Entities.DomainViagem")
+            //    .ToList()
+            //    .ForEach(type =>
+            //    {
+            //        dynamic instance = Activator.CreateInstance(type);
+            //        modelBuilder.Configurations.Add(instance);
+            //    });
+            #endregion
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ backend/DivinusBurguer/Divinus.Infra/Persistence/Map/MapFood.cs
@@ -0,0 +1,17 @@
+﻿using Divinus.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Divinus.Infra.Persistence.Map
+{
+    public class MapFood : EntityTypeConfiguration<Food>
+    {
+        public MapFood()
+        {
+            ToTable("Food");
+            Property(p => p.Name).HasMaxLength(250).IsRequired();
+            Property(p => p.Description).HasMaxLength(250).IsRequired();
+            Property(p => p.Price).IsRequired();
+            Property(p => p.ImageName).HasMaxLength(250).IsRequired();
+        }
+    }
+}
