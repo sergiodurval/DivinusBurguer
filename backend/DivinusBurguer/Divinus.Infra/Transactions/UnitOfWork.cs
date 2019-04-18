4b2c9badb2587198dd9bb5e188262dc30f42b7de
SergioDurval <sergiodurval2@gmail.com>
2019-04-17 23:04:47 -0300

    Criando a camada de IOC

---
+++ backend/DivinusBurguer/Divinus.Infra/Transactions/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Divinus.Infra.Persistence;
+
+namespace Divinus.Infra.Transactions
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DivinusContext _context;
+
+        public UnitOfWork(DivinusContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ backend/DivinusBurguer/Divinus.IoC/Unity/DependencyResolver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using Unity.Lifetime;
+using Divinus.Infra.Persistence;
+using prmToolkit.NotificationPattern;
+using Divinus.Domain.Interfaces.Services;
+using Divinus.Domain.Services;
+using Divinus.Domain.Interfaces.Repositories.Base;
+using Divinus.Infra.Persistence.Repositories.Base;
+using Divinus.Domain.Interfaces.Repositories;
+using Divinus.Infra.Persistence.Repositories;
+using Divinus.Infra.Transactions;
+
+namespace Divinus.IoC.Unity
+{
+    public static class DependencyResolver
+    {
+        public static void Resolve(UnityContainer container)
+        {
+
+            container.RegisterType<DbContext,DivinusContext>(new HierarchicalLifetimeManager());
+            //UnitOfWork
+            container.RegisterType<IUnitOfWork, UnitOfWork>(new HierarchicalLifetimeManager());
+            container.RegisterType<INotifiable, Notifiable>(new HierarchicalLifetimeManager());
+
+            //Serviço de Domain
+            //container.RegisterType(typeof(IServiceBase<,>), typeof(ServiceBase<,>));
+
+            container.RegisterType<IServiceFood, ServiceFood>(new HierarchicalLifetimeManager());
+            
+
+
+            //Repository
+            container.RegisterType(typeof(IRepositoryBase<,>), typeof(RepositoryBase<,>));
+
+            container.RegisterType<IRepositoryFood, RepositoryFood>(new HierarchicalLifetimeManager());
+            
+
+
+
+        }
+    }
+}
