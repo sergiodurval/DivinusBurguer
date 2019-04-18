07aecdf1d757b5da19bb3dce19a99c3a693a74d5
SergioDurval <sergiodurval2@gmail.com>
2019-04-18 00:06:11 -0300

    Inicio da construcao da api

---
+++ backend/DivinusBurguer/Divinus.Api/Security/AuthorizationProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Unity;
+
+namespace Divinus.Api.Security
+{
+    public class AuthorizationProvider : OAuthAuthorizationServerProvider
+    {
+        private readonly UnityContainer _container;
+
+        public AuthorizationProvider(UnityContainer container)
+        {
+            _container = container;
+        }
+    }
+}
