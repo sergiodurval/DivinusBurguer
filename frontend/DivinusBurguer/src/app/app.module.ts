c420eaeec9f6d3c9d0d8664f676ba14146afe310
SergioDurval <sergiodurval2@gmail.com>
2019-05-06 23:58:10 -0300

    Criando componente home e arquivo de rotas

--- frontend/DivinusBurguer/src/app/app.module.ts
+++ frontend/DivinusBurguer/src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {ROUTES} from './app.routes'
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
