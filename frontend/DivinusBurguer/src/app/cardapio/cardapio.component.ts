3d9b6dfeed3364b9fdf63a5ab0885faaf875b727
SergioDurval <sergiodurval2@gmail.com>
2019-06-08 12:45:37 -0300

    Correcao do bug da duplicidade da notificacao

--- frontend/DivinusBurguer/src/app/cardapio/cardapio.component.ts
+++ frontend/DivinusBurguer/src/app/cardapio/cardapio.component.ts
@@ -3,7 +3,7 @@ import { Food } from './food.model';
 import { FoodService } from './food.service';
 import { NotificacaoService } from 'app/notificacao/notificacao.service';
 import { CarrinhoService } from 'app/carrinho/carrinho.service';
-
+import {ToastyService, ToastyConfig, ToastOptions, ToastData} from 'ng2-toasty';
 
 @Component({
   selector: 'app-cardapio',
@@ -18,7 +18,9 @@ export class CardapioComponent implements OnInit {
   itemSelecionado : Food
 
   constructor(private foodService:FoodService , private notificationService:NotificacaoService,
-              private carrinhoService:CarrinhoService) { }
+              private carrinhoService:CarrinhoService,
+              private toastyService:ToastyService, 
+              private toastyConfig: ToastyConfig) { }
 
   ngOnInit() {
     this.foodService.foods()
@@ -27,10 +29,8 @@ export class CardapioComponent implements OnInit {
 
 
   addCart(id:string):void{
-     this.notificationService.notificar(this.getName(id));
-     this.itemSelecionado = this.getFood(id)
-     console.log(this.itemSelecionado)
-     this.carrinhoService.adicionarCarrinho(this.itemSelecionado)
+     this.carrinhoService.adicionarCarrinho(this.getFood(id))
+     this.addToast(this.getName(id))
   }
 
   getName(id:string):string{
@@ -47,6 +47,24 @@ export class CardapioComponent implements OnInit {
          return food;
        }
     }
+  }
+
+  addToast(title:string) {
+    var toastOptions:ToastOptions = {
+        title: title,
+        msg: "Adicionado ao carrinho",
+        showClose: true,
+        timeout: 2000,
+        theme: 'bootstrap',
+        onAdd: (toast:ToastData) => {
+            console.log('Toast ' + toast.id + ' has been added!');
+        },
+        onRemove: function(toast:ToastData) {
+            console.log('Toast ' + toast.id + ' has been removed!');
+        }
+    };
+    
+    this.toastyService.success(toastOptions);
   }
 
 }
--- frontend/DivinusBurguer/src/app/notificacao/notificacao.component.ts
+++ frontend/DivinusBurguer/src/app/notificacao/notificacao.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ToastyService, ToastyConfig, ToastOptions, ToastData} from 'ng2-toasty';
 import { NotificacaoService } from './notificacao.service';
 
+
 @Component({
   selector: 'app-notificacao',
   templateUrl: './notificacao.component.html',
@@ -9,38 +10,18 @@ import { NotificacaoService } from './notificacao.service';
 })
 export class NotificacaoComponent implements OnInit {
   
-  title : string
+ 
 
-  constructor(private toastyService:ToastyService, private toastyConfig: ToastyConfig,
-    private notificationService:NotificacaoService) { 
-    this.toastyConfig.theme = 'bootstrap';
- }
+  constructor() { }
 
   ngOnInit() {
-    this.notificationService.notificacao.subscribe(message => {
-      this.title = message
-      this.addToast()
-    })
+    
   }
 
  
-  addToast() {
-    var toastOptions:ToastOptions = {
-        title: this.title,
-        msg: "Adicionado ao carrinho",
-        showClose: true,
-        timeout: 2000,
-        theme: 'bootstrap',
-        onAdd: (toast:ToastData) => {
-            console.log('Toast ' + toast.id + ' has been added!');
-        },
-        onRemove: function(toast:ToastData) {
-            console.log('Toast ' + toast.id + ' has been removed!');
-        }
-    };
+  
     
-    this.toastyService.success(toastOptions);
 
 }
 
-}
+
